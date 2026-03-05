f18503674d0fc9766e7bcf03a934019d5558c63d
Gabrielle Joei Bactong <gabriellejoeibactong@Gabrielles-MacBook-Pro.local>
2026-03-06 01:27:15 +0800

    Feat: Comment.ts and User.ts

--- catpuccino/models/User.ts
+++ catpuccino/models/User.ts
@@ -1,6 +1,10 @@
 import mongoose, { Schema, model, models} from 'mongoose'; 
 
 const UserSchema = new Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, 'User ID is required'],
+    },
     username: {
         type: String, 
         required: [true, 'Username is required'], 
@@ -15,16 +19,54 @@ const UserSchema = new Schema({
         type: String, 
         required: [true, 'Password is required'],
     }, 
-    bio: {
-        type: String, 
-        default: "Meow meow meow mewo...", 
+    role: {
+        type: [String], 
+        enum: ["user", "owner", "admin"],
+        default: ["user"],
     },
-    profilePic: {
-        type: String, 
-        default: "/default-avatar.png",
+    profile: {
+        firstName: { 
+            type: String, 
+            required: true 
+        },
+        lastName: { 
+            type: String, 
+            required: true 
+        },
+        profilePicURL: { 
+            type: String, 
+            default: null 
+        },
+        coverPicURL: { 
+            type: String, 
+            default: null 
+        },
+        bio: { 
+            type: String, 
+            default: "" 
+        },
+        shortDescription: { 
+            type: String, 
+            default: ""
+        },
+    },
+    isDeactivated: {
+        type: Boolean,
+        default: false,
+    },
+    rememberToken: { 
+        type: String 
+    },
+    tokenExpiration: { 
+        type: Date 
+    },
+    favoriteCatCafeID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'CatCafe',
+        required: [true, 'Favorite Cat Cafe is required'],
     }
     }, {
-    timestamps: true, //To automatically add timestamps for "createdAt" and "updatedAt"
+    timestamps: true // Automatically handles createdAt and updatedAt
 }); 
 
 // Don't delete this one 
