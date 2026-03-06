3b2b6a73e415952d723923e5ea7b6e9862ae9e32
Catherine-Gunita <catherine_gunita@dlsu.edu.ph>
2026-03-06 09:58:35 +0800

    Added set server to fix the error

--- catpuccino/lib/mongodb.ts
+++ catpuccino/lib/mongodb.ts
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL;
+import { setServers } from "node:dns/promises";
+
+setServers(["8.8.8.8", "1.1.1.1"]);
+const MONGODB_URL = process.env.MONGODB_URI;
 
 if (!MONGODB_URL) {
-  throw new Error('Please define the MONGODB_URL environment variable in .env');
+  throw new Error('Please define the MONGODB_URI environment variable in .env');
 }
 
 export const connectDB = async () => {
   try {
+    mongoose.set('strictQuery', false);
     if (mongoose.connection.readyState >= 1) return;
     await mongoose.connect(MONGODB_URL);
     console.log("Successfully connected to Catpuccino Database");
